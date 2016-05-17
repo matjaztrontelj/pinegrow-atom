aceda49a7ccaff62713dae8903a76c179d279b01
Matjaz <matjaz.trontelj@gmail.com>
2016-05-17 15:39:00 +0200

    0.0.1

---
+++ spec/pinegrow-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PinegrowAtomView from '../lib/pinegrow-atom-view';
+
+describe('PinegrowAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
